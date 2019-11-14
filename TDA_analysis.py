bfa433b6537d8dc4eb0cffc3f9275451a07e24fb
CarlosLing <37625533+CarlosLing@users.noreply.github.com>
2019-11-14 14:42:17 +0100

    Add standard deviation for normalized signature

--- TDA_analysis.py
+++ TDA_analysis.py
@@ -1,19 +1,56 @@
 import stableRANK as sr
-
+from distance import get_distance_matrix
 import numpy as np
 inf=float("inf")
 
-import matplotlib.pyplot as plt
-plt.style.use('ggplot')
 
-import _pickle as pickle
-from ripser import ripser
+def tda_analysis(data, variable='Income', N=50, N_sig=20):
 
-data = np.random.random((100,2))
-diagrams = ripser(data)['dgms']
-plot_diagrams(diagrams, show=True)
+    # Do analysis of signatures regarding variable
+    types = data[variable].unique()
 
-rips = Rips()
-data = np.random.random((100,2))
-diagrams = rips.fit_transform(data)
-rips.plot(diagrams)
+    results = dict()
+    contours = {"H0": [[[0], [1]], ("area", inf, inf)]}
+
+    for t in types:
+
+        data_subset = data[data[variable] == t]
+        barcodes = list()
+        signatures = list()
+        normalized_sig = list()
+        sum_H0 = 0
+        sum_H1 = 0
+        sum_norm = 0
+        for i in range(N_sig):
+            distance_matrix, sampled_data = get_distance_matrix(data_subset, N)
+            ob = sr.euc_object(points=distance_matrix)
+
+            bc = ob.barcode(distance_matrix=True)
+            sign = bc.stable_rank_max(contours)
+            n_sig = sign["H1"] * (sign["H0"].invert())
+
+            barcodes.append(bc)
+            signatures.append(sign)
+            normalized_sig.append(n_sig)
+
+            sum_H0 = sum_H0 + sign['H0']
+            sum_H1 = sum_H1 + sign['H1']
+            sum_norm = sum_norm + n_sig
+
+        results[t] = {'barcodes': barcodes,
+                      'signatures': signatures,
+                      'normalized_sign': normalized_sig,
+                      'mean_H0': (1 / N_sig) * sum_H0,
+                      'mean_H1': (1 / N_sig) * sum_H1,
+                      'mean_norm': (1 / N_sig) * sum_norm}
+
+        sum_diff_sq = 0
+
+        for i in range(N_sig):
+            diff_norm = results[t]['normalized_sign'][i] - results[t]['mean_norm']
+            sum_diff_sq = sum_diff_sq + diff_norm*diff_norm
+
+        results[t]['sd_norm'] = (1 / (N_sig-1)) * sum_diff_sq
+        results[t]['sd_norm'].content[1] = np.sqrt(results[t]['sd_norm'].content[1])
+
+    return results
--- main.py
+++ main.py
@@ -14,53 +14,18 @@ plt.style.use('ggplot')
 
 import NEWDistance as dist
 from utils import read_data
+from TDA_analysis import tda_analysis
 
 if __name__ == '__main__':
+
     d = dist.Distance()
     d.weights[3] = 0.00001
     data = read_data('adult.data', gdp_file='GDP_percapita_complete.csv')
 
-    N = 50
-    N_sig = 20
-    # Do analysis of signatures regarding variable
-    variable = 'Income'
+    results = tda_analysis(data, variable='Gender')
+
+    variable = 'Gender'
     types = data[variable].unique()
-
-    results = dict()
-    contours = {"H0": [[[0], [1]], ("area", inf, inf)]}
-
-    for t in types:
-
-        data_subset = data[data[variable] == t]
-        barcodes = list()
-        signatures = list()
-        normalized_sig = list()
-        sum_H0 = 0
-        sum_H1 = 0
-        sum_norm = 0
-        for i in range(N_sig):
-
-            distance_matrix, sampled_data = get_distance_matrix(data_subset, N)
-            ob = sr.euc_object(points=distance_matrix)
-
-            bc = ob.barcode(distance_matrix=True)
-            sign = bc.stable_rank_max(contours)
-            n_sig = sign["H1"] * (sign["H0"].invert())
-
-            barcodes.append(bc)
-            signatures.append(sign)
-            normalized_sig.append(n_sig)
-
-            sum_H0 = sum_H0 + sign['H0']
-            sum_H1 = sum_H1 + sign['H1']
-            sum_norm = sum_norm + n_sig
-
-        results[t] = {'barcodes': barcodes,
-                      'signatures': signatures,
-                      'normalized_sign': normalized_sig,
-                      'mean_H0': (1/N_sig) * sum_H0,
-                      'mean_H1': (1/N_sig) * sum_H1,
-                      'mean_norm': (1/N_sig) * sum_norm}
 
     plt.figure()
     results[t]['barcodes'][0].plot()
@@ -71,11 +36,34 @@ if __name__ == '__main__':
     plt.figure()
     results[types[1]]['mean_norm'].plot()
     plt.show()
+
+    t = types[0]
+    N_sig = 20
+    plt. figure()
+    for i in range(N_sig):
+        results[types[0]]['normalized_sign'][i].plot(color='red')
+        results[types[1]]['normalized_sign'][i].plot(color='blue')
+    plt.show()
+
+    t = types[0]
     plt.figure()
-    results[t]['signatures'][0]['H1'].plot()
+    upper = results[t]['mean_norm'] + results[t]['sd_norm']
+    lower = results[t]['mean_norm'] - results[t]['sd_norm']
+    upper.plot(color='red')
+    results[t]['mean_norm'].plot(color='red')
+    lower.plot(color='red')
+    t = types[1]
+    upper = results[t]['mean_norm'] + results[t]['sd_norm']
+    lower = results[t]['mean_norm'] - results[t]['sd_norm']
+    upper.plot(color='blue')
+    results[t]['mean_norm'].plot(color='blue')
+    lower.plot(color='blue')
     plt.show()
     plt.figure()
-    results[t]['normalized_sign'][0].plot()
+    results[t]['sd_norm'].plot()
+    plt.show()
+    plt.figure()
+    results[types[0]]['normalized_sign'][0].plot()
     plt.show()
 
     ### Here comes the analysis:
