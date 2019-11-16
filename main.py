904642604f4686e80f05c4cb665d589cbf7777fe
FatMansKryptonite <ivar.c.eriksson@hotmail.com>
2019-11-16 12:36:15 +0100

    Removed Distance Test

--- main.py
+++ main.py
@@ -92,22 +92,3 @@ if __name__ == '__main__':
     normelized_sig = signatures["H1"] * (signatures["H0"].invert())
     normelized_sig.plot()
     plt.show()
-    
-    # ----------Distance Test----------
-    print("Distance Test:")
-    d1 = data.iloc[0]
-    d2 = data.iloc[1]
-    d3 = data.iloc[2]
-    d1v = [d1.Age, d1.EducationNum, d1.HoursPerWeek, d1.NetCapital, d1.Gender, d1.Race,
-           d1.WorkClass, d1.MaritalStatus, d1.Occupation, d2.Relationship, d1.NativeCountry]
-    d2v = [d2.Age, d2.EducationNum, d2.HoursPerWeek, d2.NetCapital, d2.Gender, d2.Race,
-           d2.WorkClass, d2.MaritalStatus, d2.Occupation, d2.Relationship, d2.NativeCountry]
-    d3v = [d3.Age, d3.EducationNum, d3.HoursPerWeek, d3.NetCapital, d3.Gender, d3.Race,
-           d3.WorkClass, d3.MaritalStatus, d3.Occupation, d3.Relationship, d3.NativeCountry]
-    print(d.standard_dist(d1v, d2v))
-    print(d.standard_dist(d1v, d3v))
-    print(d.standard_dist(d2v, d3v))
-    d.weights *= 2;
-    print(d.standard_dist(d1v, d2v))
-    print(d.standard_dist(d1v, d3v))
-    print(d.standard_dist(d2v, d3v))
